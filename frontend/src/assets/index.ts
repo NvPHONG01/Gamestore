bf9c375eeeed41d3bf00baf4d09cde327a6f65cf
PhongNV <Thieugialee01@gmail.com>
2022-12-21 17:34:20 +0700

    footer header

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,1 +1,15 @@
 export const img_AppStore = require("../assets/imgs/logo_download_on_the_appstore.png")
+
+const slider_1 = require("../assets/imgs/slider_1.jpg")
+const slider_2 = require("../assets/imgs/slider_2.jpg")
+const slider_3 = require("../assets/imgs/slider_3.jpg")
+
+export const Slider = [
+  slider_1,
+  slider_2,
+  slider_3
+]
+
+
+
+export const slide = require("../assets/imgs/slider_3.jpg")
--- frontend/src/components/Main/index.tsx
+++ frontend/src/components/Main/index.tsx
@@ -1,9 +1,16 @@
+import { slide } from "../../assets";
+
 const Main = () => {
   return (
     <div style={{ backgroundColor: '#fff', justifyContent: "center", display: "flex" }}>
       <div style={{ width: 1440, height: 592, justifyContent: 'center', display: 'flex' }}>
         <div style={{ width: 1201, display: 'flex', flexDirection: 'column', height: 316 }}>
-          
+          {/* {Slider.filter(x => */}
+          <div style={{ width: 1201, height: 316, background: `${slide}` }}>
+
+          </div>
+          <img src={slide} alt="" />
+          {/* )} */}
         </div>
       </div>
     </div>
